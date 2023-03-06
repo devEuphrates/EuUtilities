450af5dceb7caff9bc550efbfd8d0eaf63115d7a
Fırat Yıldırım <frat.yldrm.11@gmail.com>
2023-03-06 19:45:40 +0300

    Temporary Fix To Singleton

--- Runtime/Extra/Singleton.cs
+++ Runtime/Extra/Singleton.cs
@@ -3,21 +3,7 @@ using UnityEngine;
 public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
 {
     static T _instance;
-    public static T Instance 
-    {
-        get 
-        {
-            if (_instance != null)
-                return _instance;
-
-            GameObject go = new GameObject(typeof(T).Name);
-
-            T comp = go.AddComponent<T>();
-            _instance = comp;
-
-            return _instance;
-        }
-    }
+    public static T Instance => _instance;
 
     protected virtual void Awake()
     {
