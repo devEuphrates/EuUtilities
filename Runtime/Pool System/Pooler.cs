c02c56068be9d9c8ff703ddf273a616146f5f15c
Fırat Yıldırım <frat.yldrm.11@gmail.com>
2023-03-18 15:28:56 +0300

    Pool Pre Instantiation Fixed and Made Async

--- Runtime/Pool System/Pooler.cs
+++ Runtime/Pool System/Pooler.cs
@@ -1,14 +1,21 @@
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.Pool;
 
 public class Pooler : Singleton<Pooler>
 {
+    public static readonly int PRE_INSTANTIATE_BATCH = 25;
+    public delegate void PreInstantiateEvent(string poolName, int instantiatedAmount);
+    public static event PreInstantiateEvent OnPoolPreInstantiateComplete;
+
+    public static Transform ObjParent => Pooler.Instance.transform;
+
     // Data of objects to be pooled.
-    [SerializeField] List<Data> _pooledObjects = new List<Data>();
+    [SerializeField] List<PoolData> _pooledObjects = new List<PoolData>();
 
     // Dictionary that will be populated with pools. Keys are set to names from _pooledObjects list.
-    Dictionary<string, ObjectPool<GameObject>> _pools = new Dictionary<string, ObjectPool<GameObject>>();
+    Dictionary<string, IObjectPool<GameObject>> _pools = new Dictionary<string, IObjectPool<GameObject>>();
 
     // Collection check setting is global unlike pool size so it can be enabled only if the code is consistent.
     [SerializeField] bool _collectionCheck = false;
@@ -22,32 +29,47 @@ public class Pooler : Singleton<Pooler>
         // Initializ pool of each data set and populate the dictionary.
         foreach (var dt in _pooledObjects)
         {
-            var pool = (_pools[dt.Name] = new ObjectPool<GameObject>(() => CreatePoolObject(dt.Name, dt.Prefab), OnObjectTaken, OnObjectReturned, OnDestroyObject
-                , _collectionCheck, dt.MinCapacity, dt.MaxCapacity));
+            var pool = (_pools[dt.Name] = new LinkedPool<GameObject>(dt.CreateObject, OnObjectTaken, OnObjectReturned, OnDestroyObject
+                , _collectionCheck, dt.MaxCapacity));
 
             //  We get and release min capacity amount of objects to pre spawn min amount of objects and deactivate them.
 
-            for (int i = 0; i < dt.MinCapacity; i++)
-                spawned.Add(pool.Get());
+            if (dt.PreInstantiate < 1)
+            {
+                OnPoolPreInstantiateComplete?.Invoke(dt.Name, 0);
+                return;
+            }
 
-            foreach (var item in spawned)
-                pool.Release(item);
+            PreInstantiate(dt.Name, dt.PreInstantiate);
+
+            //for (int i = 0; i < dt.MinCapacity; i++)
+            //    spawned.Add(pool.Get());
+
+            //foreach (var item in spawned)
+            //    pool.Release(item);
         }
-
     }
 
-    GameObject CreatePoolObject(string name, GameObject prefab)
+    async void PreInstantiate(string poolName, int amount)
     {
-        // Spawn the object and set the name so it's pool can be identified from the inspector.
-        GameObject go = Instantiate(prefab);
-        go.name = $"Pooled {name}";
+        if (!_pools.TryGetValue(poolName, out var pool))
+            return;
 
-        // Set pooler as parent so it does not populate scene view.
-        go.transform.SetParent(transform);
-        // Deactivate object to prevent 
-        go.SetActive(false);
+        GameObject[] spawned = new GameObject[amount];
 
-        return go;
+        for (int i = 0; i < amount; i++)
+        {
+            GameObject go = pool.Get();
+            spawned[i] = go;
+
+            if (i % PRE_INSTANTIATE_BATCH == 0)
+                await Task.Yield();
+        }
+
+        for (int i = 0; i < amount; i++)
+            pool.Release(spawned[i]);
+
+        OnPoolPreInstantiateComplete?.Invoke(poolName, amount);
     }
 
     void OnObjectTaken(GameObject go)
@@ -82,12 +104,26 @@ public class Pooler : Singleton<Pooler>
     }
 
     [System.Serializable]
-    struct Data
+    struct PoolData
     {
         public string Name;
         public GameObject Prefab;
-        public int MinCapacity;
+        public int PreInstantiate;
         public int MaxCapacity;
+
+        public GameObject CreateObject()
+        {
+            // Spawn the object and set the name so it's pool can be identified from the inspector.
+            GameObject go = Instantiate(Prefab);
+            go.name = $"Pooled {Name}";
+
+            // Set pooler as parent so it does not populate scene view.
+            go.transform.SetParent(ObjParent);
+            // Deactivate object to prevent 
+            go.SetActive(false);
+
+            return go;
+        }
     }
 
     // This static methode handles spawning from existing _pools.
